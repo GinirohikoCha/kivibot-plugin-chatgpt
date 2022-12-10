72cc02237a8c54f9bea2226bf5734b6285fc41ec
Viki <hi@viki.moe>
2022-12-10 09:25:23 +0800

    feat: 自动刷新 token，自动检测无效

--- README.md
+++ README.md
@@ -4,6 +4,13 @@
 
 `KiviBot` 的 [ChatGPT](https://chat.openai.com) 插件。
 
+**特征**
+
+- 可选配置触发指令前缀，默认为 `%`
+- 默认开启上下文会话模式
+- 根据群聊、私聊进行会话隔离，互不干扰
+- 每天四点以及请求失败时，尝试自动刷新 `sessionToken`
+
 **安装**
 
 使用框架消息指令进行安装
@@ -12,11 +19,28 @@
 /plugin add chatgpt
 ```
 
+启用插件，生成初始配置文件
+
+```shell
+/plugin on chatgpt
+```
+
 **配置**
 
-编辑 `data/plugins/ChatGPT/config.json` 文件，配置 `sessionToken`。
+编辑 Bot 目录下的 `data/plugins/ChatGPT/config.json` 文件，配置 `sessionToken` 字段。
 
-`sessionToken` 获取方式参考：[chatgpt-api](https://github.com/transitive-bullshit/chatgpt-api#session-tokens)。
+`sessionToken` 获取方式：
+
+1. 打开 https://chat.openai.com/chat 进行登录/注册。
+2. 打开开发者工具（桌面端浏览器按 `F12` 可以打开）。
+3. 切换到 Application（应用）选项卡 > Cookie 子选项卡。
+4. 找到 `__Secure-next-auth.session-token` 的值，这个就是 `sessionToken`，双击并复制。
+
+如图所示：
+
+![dev tools](./docs/devtool.png)
+
+> derived from: [chatgpt-api](https://github.com/transitive-bullshit/chatgpt-api#session-tokens)
 
 ```jsonc
 {
@@ -27,10 +51,16 @@
 }
 ```
 
-**启用**
+**应用**
 
-通过消息命令启用 ChatGPT 插件。
+配置完成后，通过消息命令重载 ChatGPT 插件。
 
 ```shell
-/plugin on chatgpt
+/plugin reload chatgpt
 ```
+
+**使用截图**
+
+![h1](docs/chat_history_1.jpg)
+![h2](docs/chat_history_2.jpg)
+![h3](docs/chat_history_3.jpg)
--- index.js
+++ index.js
@@ -1,35 +1,63 @@
 const { KiviPlugin } = require('@kivibot/core')
 
-const plugin = new KiviPlugin('ChatGPT', '1.2.1')
+const plugin = new KiviPlugin('ChatGPT', '1.3.0')
 
 const config = {
+  // sessionToken
   sessionToken: '',
+  // 命令前缀
   cmdPrefix: '%'
 }
 
 const map = new Map()
+
+const msgs = {
+  needConfig: `ChatGPT: 第一次使用请先在 Bot 目录下的 data/plugin/${plugin.name}/config.json 中配置 sessionToken，抓取方式参考：https://github.com/KiviBotLab/kivibot-plugin-chatgpt，完成后重载插件`,
+  expired: 'ChatGPT: sessionToken 可能已失效，请重新抓取并配置后重载插件',
+  timeout: 'ChatGPT: 加载超时，请稍后再试试吧'
+}
+
+const ChromeUA =
+  'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36'
 
 plugin.onMounted(async bot => {
   Object.assign(config, plugin.loadConfig())
   plugin.saveConfig(config)
 
   if (!config.sessionToken) {
-    bot.sendPrivateMsg(plugin.mainAdmin, 'ChatGPT: 请先配置 sessionToken，完成后重载插件')
-    plugin.throwPluginError('请先配置 sessionToken，完成后重载插件')
+    bot.sendPrivateMsg(plugin.mainAdmin, msgs.needConfig)
+    plugin.throwPluginError(msgs.needConfig)
     return
   }
 
   const { ChatGPTAPI } = await import('chatgpt')
-  const api = new ChatGPTAPI({ sessionToken: config.sessionToken })
-  await api.ensureAuth()
+
+  const api = new ChatGPTAPI({
+    markdown: true,
+    sessionToken: config.sessionToken,
+    userAgent: ChromeUA,
+    accessTokenTTL: 60 * 1000
+  })
+
+  try {
+    // 确保 token 有效
+    await api.ensureAuth()
+  } catch {
+    plugin.throwPluginError(msgs.expired)
+    await bot.pickFriend(plugin.mainAdmin).sendMsg(msgs.expired)
+    return
+  }
 
   plugin.onMessage(async event => {
     const { raw_message } = event
 
-    if (!raw_message.startsWith(config.cmdPrefix)) return
+    if (!raw_message.startsWith(config.cmdPrefix)) {
+      return
+    }
 
     let session
 
+    // 针对不同的群、私聊进行单独会话创建和复用
     if (event.message_type === 'group') {
       if (map.has(event.group_id)) {
         session = map.get(event.group_id)
@@ -53,13 +81,48 @@ plugin.onMounted(async bot => {
       }
     }
 
-    const msg = raw_message.replace('%', '')
+    const msg = raw_message.replace(config.cmdPrefix, '')
 
     try {
       const res = await session.sendMessage(msg, { timeoutMs: 2 * 60 * 1000 })
       event.reply(res, true)
     } catch {
-      event.reply('加载超时，稍后再试试吧', true)
+      try {
+        // 如果请求失败，尝试刷新 sessionToken 后重新请求，还是刷新失败则可能 token 过期
+        await api.refreshAccessToken()
+      } catch {
+        event.reply(msgs.expired, true)
+
+        // token 过期时，打印日志并私聊通知主管理进行更新
+        bot.pickFriend(plugin.mainAdmin).sendMsg(msgs.expired)
+        plugin.throwPluginError(msgs.expired)
+
+        return
+      }
+
+      try {
+        // token 刷新成功后再次尝试请求
+        const res = await session.sendMessage(msg, { timeoutMs: 2 * 60 * 1000 })
+        event.reply(res, true)
+      } catch (e) {
+        plugin.throwPluginError(e)
+        event.reply(msgs.timeout, true)
+      }
+    }
+  })
+
+  /**
+   * 凌晨四点刷新一次 sessionToken
+   * @from https://github.com/easydu2002/chat_gpt_oicq/blob/dev/src/main.ts#L23-L28
+   */
+  const refreshTask = plugin.cron('0 0 4 * * *', async bot => {
+    try {
+      await api.refreshAccessToken()
+    } catch {
+      await bot.pickFriend(plugin.mainAdmin).sendMsg(msgs.expired)
+      plugin.throwPluginError(msgs.expired)
+
+      refreshTask.stop()
     }
   })
 })
