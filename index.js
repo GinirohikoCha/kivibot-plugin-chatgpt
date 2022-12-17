90e75b69ce7e5f229cdd5bf1c97163b736abe40b
GinirohikoCha <rinkouatf@qq.com>
2022-12-18 01:56:34 +0800

    更新到v2版本，移除chatgpt-api依赖 添加api上下文支持，添加上下文过期为一小时

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
 const { KiviPlugin } = require('@kivibot/core')
 
 const { version } = require('./package.json')
-const plugin = new KiviPlugin('ChatGPT', version)
+const plugin = new KiviPlugin('ChatGPTv2', version)
 
 const config = {
-  // OpenAI 邮箱账号
-  email: '',
-  // OpenAI 账号密码
-  password: '',
+  // OpenAI apiKey
+  apiKey: '',
   // 触发命令前缀
   cmdPrefix: '%'
 }
@@ -15,8 +13,8 @@ const config = {
 const map = new Map()
 
 const msgs = {
-  needConfig: `ChatGPT: 第一次使用请先在 Bot 目录下的 data/plugin/${plugin.name}/config.json 中配置 OpenAI 账号密码，OpenAI 官网：https://openai.com/`,
-  error: 'ChatGPT: 验证失败，可能是 OpenAI 账号密码错误',
+  needConfig: `ChatGPT: 第一次使用请先在 Bot 目录下的 data/plugin/${plugin.name}/config.json 中配置 apiKey，OpenAI 官网：https://openai.com/`,
+  error: 'ChatGPT: 验证失败，可能是 OpenAI apiKey错误',
   timeout: 'ChatGPT: 加载超时，请稍后再试试吧'
 }
 
@@ -24,26 +22,19 @@ plugin.onMounted(async bot => {
   Object.assign(config, plugin.loadConfig())
   plugin.saveConfig(config)
 
-  if (!config.email || !config.password) {
+  if (!config.apiKey) {
     bot.sendPrivateMsg(plugin.mainAdmin, msgs.needConfig)
     plugin.throwPluginError(msgs.needConfig)
     return
   }
 
-  const { ChatGPTAPI, getOpenAIAuth } = await import('chatgpt')
+  const { Configuration, OpenAIApi } = await import('openai')
 
-  const openAIAuth = await getOpenAIAuth({
-    email: config.email,
-    password: config.password
-  })
+  const configuration = new Configuration({
+    apiKey: config.apiKey,
+  });
 
-  const api = new ChatGPTAPI({
-    ...openAIAuth,
-    markdown: true
-  })
-
-  // 确保 token 有效
-  await api.ensureAuth()
+  const openai = new OpenAIApi(configuration);
 
   plugin.onMessage(async event => {
     const { raw_message } = event
@@ -52,63 +43,73 @@ plugin.onMounted(async bot => {
       return
     }
 
-    let session
-
-    // 针对不同的群、私聊进行单独会话创建和复用
-    if (event.message_type === 'group') {
-      if (map.has(event.group_id)) {
-        session = map.get(event.group_id)
-      } else {
-        session = api.getConversation()
-        map.set(event.group_id, session)
-      }
-    } else if (event.message_type === 'discuss') {
-      if (map.has(event.discuss_id)) {
-        session = map.get(event.discuss_id)
-      } else {
-        session = api.getConversation()
-        map.set(event.discuss_id, session)
-      }
-    } else if (event.message_type === 'private') {
-      if (map.has(event.sender.user_id)) {
-        session = map.get(event.sender.user_id)
-      } else {
-        session = api.getConversation()
-        map.set(event.sender.user_id, session)
+    if (raw_message === '%换一个话题') {
+      switch (event.message_type) {
+        case 'private':
+          map.delete(event.sender.user_id)
+          return
+        case 'group':
+          map.delete(event.group_id)
+          return
+        case 'discuss':
+          map.delete(event.discuss_id)
+          return
       }
     }
 
-    const msg = raw_message.replace(config.cmdPrefix, '')
-
     try {
-      const res = await session.sendMessage(msg, { timeoutMs: 2 * 60 * 1000 })
-      event.reply(res, true)
+      let session
+
+      switch (event.message_type) {
+        case 'private':
+          session = getSession(event.sender.user_id)
+          break
+        case 'group':
+          session = getSession(event.group_id)
+          break
+        case 'discuss':
+          session = getSession(event.discuss_id)
+          break
+      }
+
+      const requestMsg = (session.context ? session.context + '\n' : '') +
+          'Human:' + raw_message.replace(config.cmdPrefix, '') + '\nAI:'
+
+      const response = await openai.createCompletion({
+        model: "text-davinci-003",
+        prompt: requestMsg,
+        temperature: 0.5,
+        max_tokens: 2048,
+      });
+
+      // TODO 访问失败处理
+      const responseMsg = response.data.choices[0].text.replace('\n', '')
+
+      session.context = requestMsg + responseMsg
+      session.time = new Date().getTime()
+
+      console.debug(map)
+      await event.reply(responseMsg, true)
     } catch {
-      try {
-        // 如果请求失败，尝试刷新 sessionToken 后重新请求，还是刷新失败则可能 token 过期
-        await api.refreshAccessToken()
-      } catch {
-        // token 过期时，打印日志并私聊通知主管理进行更新
-        if (event.sender.user_id !== plugin.mainAdmin) {
-          bot.pickFriend(plugin.mainAdmin).sendMsg(msgs.error)
-        }
-
-        event.reply(msgs.error, true)
-        plugin.throwPluginError(msgs.error)
-
-        return
-      }
-
-      try {
-        // token 刷新成功后再次尝试请求
-        const res = await session.sendMessage(msg, { timeoutMs: 2 * 60 * 1000 })
-        event.reply(res, true)
-      } catch (e) {
-        plugin.throwPluginError(e)
-        event.reply(msgs.timeout, true)
-      }
+      // TODO
     }
   })
 })
+
+// 获取对应上下文
+function getSession (id) {
+  let session
+  if (map.has(id)) {
+    session = map.get(id)
+    // 大于一小时则重置上下文
+    if (new Date().getTime() - session.time > 1000 * 60 * 60) {
+      session.context = ''
+    }
+  } else {
+    session = { context: '', time: new Date() }
+    map.set(id, session)
+  }
+  return session
+}
 
 module.exports = { plugin }
