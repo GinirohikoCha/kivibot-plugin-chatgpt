07876772910c6c0a037a92ccbf6e347e388b542b
Viki <hi@viki.moe>
2022-12-18 20:45:29 +0800

    feat: 迁移到 OpenAI 官方 API

--- README.md
+++ README.md
@@ -1,29 +1,25 @@
-# ChatGPT Plugin for KiviBot
+# OpenAI ChatGPT Plugin for KiviBot
 
-![npm (scoped)](https://img.shields.io/npm/v/kivibot-plugin-chatgpt?color=527dec&label=kivibot-plugin-chatgpt&style=flat-square)
+[![npm-version](https://img.shields.io/npm/v/kivibot-plugin-chatgpt?color=527dec&label=kivibot-plugin-chatgpt&style=flat-square)](https://npm.im/kivibot-plugin-chatgpt)
+[![dm](https://shields.io/npm/dm/kivibot-plugin-chatgpt?style=flat-square)](https://npm.im/kivibot-plugin-chatgpt)
 
-`KiviBot` 的 [ChatGPT](https://chat.openai.com) 插件。感谢社区的 [ChatGPT-API](https://github.com/transitive-bullshit/chatgpt-api)。
-
-**请注意！！！（2022.12.13）**
-
-[chatgpt-api](https://github.com/transitive-bullshit/chatgpt-api) 要求 `node` 版本 >= 18，且需要安装 `puppeteer` 来绕过验证 (安装比较费时)，运行要求比较严苛，且结果不容乐观，建议使用其他插件，且玩且珍惜。
+[`KiviBot`](https://beta.kivibot.com) 的 [OpenAI](https://openai.com) ChatGPT 插件。
 
 **特征**
 
 - 可选配置触发指令前缀，默认为 `%`
-- 默认开启上下文会话模式
+- 可选开启 at 触发（艾特机器人）
+- 上下文会话模式，超过十分钟没有回复，自动释放
 - 根据群聊、私聊进行会话隔离，互不干扰
+- 全消息进行配置，无需手动改配置
 
 **安装**
 
-使用框架消息指令进行安装
-
 ```shell
-# 需要安装 `puppeteer` 可能耗时非常非常非常长，建议使用其他插件
 /plugin add chatgpt
 ```
 
-启用插件，生成初始配置文件
+**启用**
 
 ```shell
 /plugin on chatgpt
@@ -31,29 +27,33 @@
 
 **配置**
 
-编辑 Bot 目录下的 `data/plugins/ChatGPT/config.json` 文件，配置 [OpenAI](https://openai.com/) 账号密码。
+申请 [OpenAI](https://openai.com/api/) 的 apiKey，然后使用以下命令自动配置
 
-```jsonc
-{
-  // cOpenAI 邮箱账号
-  "email": "",
-  // cOpenAI 账号密码
-  "password": "",
-  // 此处填入自定义指令前缀，默认 %
-  "cmdPrefix": "%"
-}
+```shell
+/chatgpt setkey <apiKey>
 ```
 
-**应用**
+配置是否艾特触发
 
-配置完成后，通过消息命令重载 ChatGPT 插件。
+```shell
+/chatgpt at <on/off>
+```
+
+配置触发前缀
+
+```shell
+/chatgpt prefix <prefix>
+```
+
+配置完成后，通过消息命令重载 ChatGPT 插件生效。
 
 ```shell
 /plugin reload chatgpt
 ```
 
-**使用截图**
+**使用**
 
-![h1](docs/chat_history_1.jpg)
-![h2](docs/chat_history_2.jpg)
-![h3](docs/chat_history_3.jpg)
+```shell
+%背诵故事静夜思
+%换个话题吧
+```
--- index.js
+++ index.js
@@ -1,26 +1,28 @@
 const { KiviPlugin } = require('@kivibot/core')
+const { Configuration, OpenAIApi } = require('openai')
 
 const { version } = require('./package.json')
-const plugin = new KiviPlugin('ChatGPTv2', version)
+const plugin = new KiviPlugin('ChatGPT', version)
 
 const config = {
   // OpenAI apiKey
   apiKey: '',
+  // 是否开启 at 触发
+  enableAt: true,
   // 触发命令前缀
   cmdPrefix: '%'
 }
 
-const map = new Map()
+// session map
+const sessionMap = new Map()
 
 const msgs = {
-  needConfig: `ChatGPT: 第一次使用请先在 Bot 目录下的 data/plugin/${plugin.name}/config.json 中配置 apiKey，OpenAI 官网：https://openai.com/`,
-  error: 'ChatGPT: 验证失败，可能是 OpenAI apiKey错误',
-  timeout: 'ChatGPT: 加载超时，请稍后再试试吧'
+  needConfig: `ChatGPT: 请先配置 apiKey，格式：/chatgpt setkey <apikey>`,
+  apiError: 'ChatGPT: API 请求异常，可能是 apiKey 错误，请查看日志'
 }
 
 plugin.onMounted(async bot => {
-  Object.assign(config, plugin.loadConfig())
-  plugin.saveConfig(config)
+  plugin.saveConfig(Object.assign(config, plugin.loadConfig()))
 
   if (!config.apiKey) {
     bot.sendPrivateMsg(plugin.mainAdmin, msgs.needConfig)
@@ -28,99 +30,133 @@ plugin.onMounted(async bot => {
     return
   }
 
-  const { Configuration, OpenAIApi } = await import('openai')
+  const configuration = new Configuration({ apiKey: config.apiKey })
+  const openai = new OpenAIApi(configuration)
 
-  const configuration = new Configuration({
-    apiKey: config.apiKey,
-  });
+  plugin.onAdminCmd('/chatgpt', (e, params, options) => {
+    const [cmd, value] = params
 
-  const openai = new OpenAIApi(configuration);
+    if (cmd === 'setkey' && value) {
+      config.apiKey = value
+      plugin.saveConfig(config)
+
+      return e.reply('apiKey 配置完成，重载插件生效', true)
+    }
+
+    if (cmd === 'at' && ['on', 'off'].includes(value)) {
+      config.enableAt = value === 'on'
+      plugin.saveConfig(config)
+
+      return e.reply(`已${config.enableAt ? '开启' : '关闭'} at 触发，重载插件生效`, true)
+    }
+
+    if (cmd === 'prefix' && value) {
+      config.cmdPrefix = value
+      plugin.saveConfig(config)
+
+      return e.reply('已修改命令触发前缀，重载插件生效', true)
+    }
+
+    const cmds = ['/chatgpt setkey <apiKey>', '/chatgpt at on/off', '/chatgpt prefix <触发前缀>']
+
+    return e.reply(cmds.join('\n'), true)
+  })
 
   plugin.onMessage(async event => {
-    const { raw_message } = event
+    const { message, raw_message } = event
 
-    if (!raw_message.startsWith(config.cmdPrefix) && !event.atme) {
+    // 消息符合命令前缀
+    const isCmd = raw_message.trim().startsWith(config.cmdPrefix)
+    // Bot 被艾特
+    const isAt = message.some(e => e.type === 'at' && e.qq === bot.uin)
+
+    // 触发条件（符合命令前缀，或者在启用艾特触发时，Bot 被艾特）
+    const isHit = isCmd || (config.enableAt && isAt)
+
+    // 过滤不触发的消息
+    if (!isHit) {
       return
     }
 
-    // 强制刷新session
-    if (raw_message === '%换一个话题') {
-      switch (event.message_type) {
-        case 'private':
-          map.delete(event.sender.user_id)
-          break
-        case 'group':
-          map.delete(event.group_id)
-          break
-        case 'discuss':
-          map.delete(event.discuss_id)
-          break
-      }
-      await event.reply('话题已更新', false)
-      return
+    const reg = /((换个?话题)|([说讲聊]点?(别|(其他))的))/
+    const shouldChangeContext = raw_message.match(reg)
+
+    // 过滤更新会话消息
+    if (shouldChangeContext) {
+      sessionMap.delete(getSessionId(event))
+      return event.reply('好的，让我们重新开始。', true)
     }
 
     try {
-      let session
+      const session = getSession(event)
 
-      switch (event.message_type) {
-        case 'private':
-          session = getSession(event.sender.user_id)
-          break
-        case 'group':
-          session = getSession(event.group_id)
-          break
-        case 'discuss':
-          session = getSession(event.discuss_id)
-          break
+      const question = raw_message
+        .replace(new RegExp(`^\\s*${config.cmdPrefix}`), '')
+        .replace(new RegExp(`@${bot.nickname}`, 'g'), '')
+        .trim()
+
+      plugin.debug(question)
+
+      const prompt = session.context ? `${session.context}\nHuman:${question}\nAI:` : question
+
+      const { data, status } = await openai.createCompletion({
+        prompt,
+        model: 'text-davinci-003',
+        temperature: 0.5,
+        max_tokens: 2048
+      })
+
+      if (status !== 200) {
+        return e.reply(msgs.apiError, true)
       }
 
-      let rawMessage
-      // cmdPrefix 优化
-      rawMessage = raw_message.startsWith('%') ? raw_message.substring(1, raw_message.length) : raw_message
-      // @ 优化
-      rawMessage = event.atme ? rawMessage.replace('@' + plugin.bot.nickname + ' ', '') : rawMessage
+      const res = data.choices[0]?.text.replace(/^\s*[?？]?\n*/, '').trim() ?? ''
 
-      // 生成请求字段 含上下文
-      const requestMsg = (session.context ? session.context + '\n' : '') +
-          'Human:' + rawMessage + '\nAI:'
+      session.context = prompt + res
 
-      const response = await openai.createCompletion({
-        model: "text-davinci-003",
-        prompt: requestMsg,
-        temperature: 0.5,
-        max_tokens: 2048,
-      });
+      event.reply(res ?? '这个 OpenAI 不知道哦', true)
+    } catch (err) {
+      plugin.throwPluginError(err?.message ?? err)
 
-      console.debug(response)
-
-      // TODO 访问失败处理
-      const responseMsg = response.data.choices[0].text.replace('\n', '')
-
-      session.context = requestMsg + responseMsg
-      session.time = new Date().getTime()
-
-      console.debug(map)
-      await event.reply(responseMsg, true)
-    } catch {
-      // TODO
+      return e.reply(msgs.apiError, true)
     }
   })
 })
 
-// 获取对应上下文
-function getSession (id) {
-  let session
-  if (map.has(id)) {
-    session = map.get(id)
-    // 大于一小时则重置上下文
-    if (new Date().getTime() - session.time > 1000 * 60 * 60) {
+function getSessionId(event) {
+  switch (event.message_type) {
+    case 'private':
+      return event.sender.user_id
+    case 'group':
+      return event.group_id
+    case 'discuss':
+      return event.discuss_id
+  }
+}
+
+function getSession(event) {
+  const id = getSessionId(event)
+  const now = Date.now()
+
+  let session = sessionMap.get(id)
+
+  if (session) {
+    const tenMinutesMs = 1000 * 60 * 10
+    const isTimeout = now - session.time >= tenMinutesMs
+    const isTextTooLong = session.context.length >= 10000
+
+    if (isTimeout || isTextTooLong) {
+      // 超过十分钟没有回复，或者聊天记录大于 1w 字，清空会话记录
       session.context = ''
+    } else {
+      // 没超过十分钟，刷新当前会话时间
+      session.time = now
     }
   } else {
-    session = { context: '', time: new Date() }
-    map.set(id, session)
+    session = { context: '', time: now }
+    sessionMap.set(id, session)
   }
+
   return session
 }
 
